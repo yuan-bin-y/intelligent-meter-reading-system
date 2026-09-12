34bf30249b7e42ace3dfebf0b6f284942f925837
RE <1755054767@qq.com>
2026-09-13 00:09:51 +0800

    基础框架

---
+++ meter-api/scr/main/java/com/byy/meterreading/MeterReadingApplication.java
@@ -0,0 +1,12 @@
+package com.byy.meterreading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeterReadingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeterReadingApplication.class, args);
+    }
+}
